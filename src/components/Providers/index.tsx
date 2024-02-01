6ef039374536e572ade31a28df607c77a283fa1a
Kamil Ziemann <kamilziemann1@gmail.com>
2024-02-01 15:28:08 +0100

    Added vercel/analytics

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { Analytics } from '@vercel/analytics/react';
 import ReactQueryClientProvider from '@/components/Providers/ReactQueryClientProvider';
 import { FC, ReactNode } from 'react';
 
 const Providers: FC<{ children: ReactNode }> = ({ children }) => (
-  <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
+  <>
+    <ReactQueryClientProvider>
+      {children}
+      <Analytics />
+    </ReactQueryClientProvider>
+  </>
 );
 
 export default Providers;
