4b548a8b19a23660965dc249530c679550eb7416
Kamil Ziemann <kamilziemann1@gmail.com>
2024-02-01 12:08:42 +0100

    Added Search with combobox and debounce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,46 @@
-import InputField from '@/components/InputField'
-import Image from 'next/image'
-import { Form } from 'react-hook-form'
+'use client';
+import { Search } from '@/components/Search';
+import { Button } from '@/components/ui/button';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Location } from '@/lib/types';
+import { cn } from '@/lib/utils';
+import { ChevronsUpDown } from 'lucide-react';
+import { useCallback, useState } from 'react';
+
+const POPOVER_WIDTH = 'w-full max-w-5xl';
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState<Location | undefined>();
+
+  const handleSetActive = useCallback((product: Location) => {
+    setSelected(product);
+    setOpen(false);
+  }, []);
+
+  const displayName = selected ? selected.name : 'Select product';
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 mt-4">
+      <div className="max-w-5xl w-full">
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              className={cn('justify-between', POPOVER_WIDTH)}
+            >
+              {displayName}
+
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent side="bottom" className={cn('PopoverContent', 'p-0', POPOVER_WIDTH)}>
+            <Search selectedResult={selected} onSelectResult={handleSetActive} />
+          </PopoverContent>
+        </Popover>
+      </div>
     </main>
-  )
+  );
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,110 @@
-export interface Product {
+export interface Location {
   id: number;
-  title: string;
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  url: string;
 }
 
-export interface SearchResponse {
-  products: Product[];
-  total: number;
-  skip: number;
-  limit: number;
+interface WeatherLocation {
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  tz_id: string;
+  localtime_epoch: number;
+  localtime: string;
+}
+
+interface WeatherCondition {
+  text: string;
+  icon: string;
+  code: number;
+}
+
+interface CurrentWeather {
+  last_updated_epoch: number;
+  last_updated: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: WeatherCondition;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  vis_km: number;
+  vis_miles: number;
+  uv: number;
+  gust_mph: number;
+  gust_kph: number;
+}
+
+interface ForecastDayCondition {
+  text: string;
+  icon: string;
+  code: number;
+}
+
+interface DayForecast {
+  maxtemp_c: number;
+  maxtemp_f: number;
+  mintemp_c: number;
+  mintemp_f: number;
+  avgtemp_c: number;
+  avgtemp_f: number;
+  maxwind_mph: number;
+  maxwind_kph: number;
+  totalprecip_mm: number;
+  totalprecip_in: number;
+  totalsnow_cm: number;
+  avgvis_km: number;
+  avgvis_miles: number;
+  avghumidity: number;
+  daily_will_it_rain: number;
+  daily_chance_of_rain: number;
+  daily_will_it_snow: number;
+  daily_chance_of_snow: number;
+  condition: ForecastDayCondition;
+  uv: number;
+}
+
+interface Astro {
+  sunrise: string;
+  sunset: string;
+  moonrise: string;
+  moonset: string;
+  moon_phase: string;
+  moon_illumination: string;
+  is_moon_up: number;
+  is_sun_up: number;
+}
+
+interface ForecastDay {
+  date: string;
+  date_epoch: number;
+  day: DayForecast;
+  astro: Astro;
+  hour: any[];
+}
+
+interface Forecast {
+  forecastday: ForecastDay[];
+}
+
+interface WeatherReport {
+  location: WeatherLocation;
+  current: CurrentWeather;
+  forecast: Forecast;
 }
